c4a8fc08750247a7381d8d80c457972b6b5aa863
Vlad Stoenescu <64587367+vladstoe@users.noreply.github.com>
2023-09-10 13:18:13 +0200

    tab name changed

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 import InfoPage from './InfoPage';
 import ProjectsPage from './ProjectsPage';
@@ -7,6 +7,10 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import PersonalProjectsPage from './PersonalProjectsPage';
 
 const App = () => {
+  useEffect(() => {
+    document.title = "Vlad's CV"; // Set your universal tab title here
+  }, []);
+
   return (
     <Router>
       <div className="app">
